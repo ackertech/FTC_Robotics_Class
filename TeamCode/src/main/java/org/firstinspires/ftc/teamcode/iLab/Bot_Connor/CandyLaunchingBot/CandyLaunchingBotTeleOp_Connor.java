f8b2f88e2ea8c836db11be04a81f09a6731acbde
connor.reddington <connor.reddington@mtbethelchristian.org>
2022-10-25 17:22:38 -0400

    LONG LIVE TACO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iLab/Bot_Connor/CandyLaunchingBot/CandyLaunchingBotTeleOp_Connor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iLab/Bot_Connor/CandyLaunchingBot/CandyLaunchingBotTeleOp_Connor.java
@@ -49,9 +49,28 @@ public class CandyLaunchingBotTeleOp_Connor extends OpMode{
     @Override
     public void stop() {
 
-        //   Bot.openHand();
+    }
+
+    public void telemetryOutput() {
+        telemetry.addLine("Rick Astley is never gonna give you up");
+        telemetry.addData("LED", "TeleOp Time: " + CandyBot.currentTime);
+        telemetry.addData("LED", "LED Timer: " + CandyBot.ledTimer);
+        telemetry.addData("LED", "LED Counter: " + CandyBot.ledCounter);
+        telemetry.addData("LED", "LED Pattern: " + CandyBot.ledPattern);
+        telemetry.addData("LED", "LED Pattern: " + CandyBot.ledLights);
+        telemetry.addData("pwr", "FL mtr: " + frontLeftSpeed);
+        telemetry.addData("pwr", "FR mtr: " + frontRightSpeed);
+        telemetry.addData("pwr", "RL mtr: " + rearLeftSpeed);
+        telemetry.addData("pwr", "RR mtr: " + rearRightSpeed);
+        telemetry.addData("Left joystick Y (gp2): ", gamepad2.left_stick_y);
+        telemetry.addData("Right joystick Y (gp2): ", gamepad2.right_stick_y);
+        telemetry.update();
 
     }
+
+
+
+
 }
 
 
