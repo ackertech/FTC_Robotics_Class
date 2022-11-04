84f92e0a58893198fb0e559ab0d00dc3a99b17a1
Truxtun_Roadkill <truxtun.boyle@mtbethelchristian.org>
2022-11-04 15:48:44 -0400

    Student Packages Create for hosting code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_Roadkill/Roadkill_TeleOp_Truxtun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_Roadkill/Roadkill_TeleOp_Truxtun.java
@@ -4,56 +4,56 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Roadkill_TeleOp_Truxtun extends OpMode {
-    public double speedMultiply = 0.50;
 
+    public double speedMultiply = 0.50;
     public Roadkill_Truxtun Bot = new Roadkill_Truxtun();
 
-@Override
+    @Override
     public void init() {
-    Bot.initRobot(hardwareMap);
+
+        Bot.initRobot(hardwareMap);
     }
 
     @Override
     public void loop() {
-
-    drive();
-    speedControl();
-
-}
+        drive();
+        speedControl();
+    }
 
 public void speedControl() {
 
-    if(gamepad1.dpad_right) {
+    if (gamepad1.dpad_right) {
         speedMultiply = .25;
+    } else if (gamepad1.dpad_down) {
+        speedMultiply = .50;
+    } else if (gamepad1.dpad_left) {
+        speedMultiply = .75;
+    } else if (gamepad1.dpad_up) {
+        speedMultiply = 1.0;
     }
-        else if(gamepad1.dpad_down) {
-            speedMultiply = .50;
-    }
-        else if(gamepad1.dpad_left) {
-            speedMultiply = .75;
-    }
-        else if(gamepad1.dpad_up) {
-            speedMultiply = 1.0;
-    }
+}
 
 
     public void drive() {
-        if (gamepad1.left_stick_y > 0.1) ;
-        Bot.driveForward(speedMultiply * gamepad1.left_stick_y);
-    }
+        if (gamepad1.left_stick_y > 0.1) {
+            Bot.driveForward(speedMultiply * gamepad1.left_stick_y);
+        }
         else if (gamepad1.left_stick_y < -0.1) {
-        Bot.driveBackward(speedMultiply * gamepad1.left_stick_y);
-    }
-                else if (gamepad1.left_stick_x > 0.1) {
+
+            Bot.driveBackward(speedMultiply * gamepad1.left_stick_y);
+        }
+        else if (gamepad1.left_stick_x > 0.1) {
+
                     Bot.rotateRight(speedMultiply * gamepad1.left_stick_x);
 
         }
-                else if (gamepad1.left_stick_x < -0.1) {
-                    Bot.rotateLeft(speedMultiply * gamepad1.left_stick_x);
+        else if (gamepad1.left_stick_x < -0.1) {
+            Bot.rotateLeft(speedMultiply * gamepad1.left_stick_x);
+        }
+
+        else {
+            Bot.stopMotors();
+        }
     }
-                else {
-                    Bot.stopMotors();
-    }
-}
 
 }
